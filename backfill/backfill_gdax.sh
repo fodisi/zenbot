5d2d501bde70e1111ff3bc7a0d44947fac964bea
fodisi <fodisi@users.noreply.github.com>
2019-01-29 21:29:20 -0500

    added gdax backfill script

---
+++ backfill/backfill_gdax.sh
@@ -0,0 +1,10 @@
+zenbot backfill gdax.BTC-USD --days=1100
+zenbot backfill gdax.ETH-USD --days=1100
+zenbot backfill gdax.LTC-USD --days=1100
+zenbot backfill gdax.BCH-USD --days=1100
+zenbot backfill gdax.ETC-USD --days=1100
+zenbot backfill gdax.ZRX-USD --days=1100
+
+
+
+
